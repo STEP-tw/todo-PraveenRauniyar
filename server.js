3e868d9a463a40b28893ec474db5792468d55e84
PRAVEEN KUMAR GUPTA <guptapraveen@gmail.com>
2018-01-12 20:02:06 +0530

    Added :Login page Added

---
+++ server.js
@@ -0,0 +1,135 @@
+let fs = require('fs');
+const timeStamp = require('./time.js').timeStamp;
+const http = require('http');
+const WebApp = require('./webapp');
+
+let registered_users = [{
+  userName: 'Praveen',
+  Password: 'Guptaji'
+}, {
+  userName: 'Manish',
+  Password: 'Yadavji'
+}];
+
+let toString = o => JSON.stringify(o, null, 2);
+
+const setContentType = function(filePath) {
+  let lastIndexOfDot = filePath.lastIndexOf(".");
+  let contentType = filePath.substr(lastIndexOfDot);
+  let type = {
+    "/": "text/html",
+    ".html": "text/html",
+    ".css": "text/css",
+    ".js": "text/javascript",
+    ".jpg": "image/jpg",
+    ".gif": "image/gif",
+    ".pdf": "text/pdf",
+    ".ico": "image/ico"
+  };
+  return type[contentType];
+};
+
+let logRequest = (req, res) => {
+  let text = ['------------------------------',
+    `${timeStamp()}`,
+    `${req.method} ${req.url}`,
+    `HEADERS=> ${toString(req.headers)}`,
+    `COOKIES=> ${toString(req.cookies)}`,
+    `BODY=> ${toString(req.body)}`, ''
+  ].join('\n');
+  fs.appendFile('request.log', text, () => {});
+};
+
+let loadUser = (req, res) => {
+  let sessionid = req.cookies.sessionid;
+  let user = registered_users.find(u => u.sessionid == sessionid);
+  if (sessionid && user) {
+    req.user = user;
+  }
+};
+
+let redirectLoggedInUserToHome = (req, res) => {
+  if (req.urlIsOneOf(['/login']) && req.user) res.redirect('homePage.html');
+};
+
+let redirectLoggedOutUserToLogin = (req, res) => {
+  if (req.urlIsOneOf(['/logout']) && !req.user) res.redirect('/login');
+};
+
+let app = WebApp.create();
+app.use(logRequest);
+app.use(loadUser);
+app.use(redirectLoggedInUserToHome);
+app.use(redirectLoggedOutUserToLogin);
+
+const getLoginPage = function(req,res) {
+  if (req.cookies.logInFailed)
+  res.write('<p>logIn Failed</p>');
+  let filePath = "./public/login.html";
+  let loginPageContent = fs.readFileSync(filePath, "utf8")
+  res.write(loginPageContent);
+};
+
+app.get('/login', (req, res) => {
+  if(req.user) {
+    res.redirect('/homePage.html');
+    return;
+  }
+  res.setHeader('Content-type', "text/html");
+  getLoginPage(req,res);
+  res.end();
+});
+
+app.post('/login', (req, res) => {
+  let user = registered_users.find(u => u.userName == req.body.userName && u.Password == req.body.Password);
+  console.log(user);
+  if (!user) {
+    res.redirect('/login');
+    return;
+  };
+  let sessionid = new Date().getTime();
+  res.setHeader('Set-Cookie', `sessionid=${sessionid}`);
+  user.sessionid = sessionid;
+  res.redirect('/homePage.html');
+});
+
+
+// app.get('/logout', (req, res) => {
+//   if(!req.user) {
+//     res.redirect('/login');
+//     return;
+//   }
+//   res.setHeader('Set-Cookie', [`logInFailed=false`, `sessionid=0,Expires=${new Date(1).toUTCString()}`]);
+//   delete req.user.sessionid;
+//   res.redirect('/login');
+// });
+
+const getFilePath = function (req) {
+  if (req.url == "/") {
+    return `./public/index.html`;
+  };
+  return `./public${req.url}`;
+};
+
+app.postuse((req, res) => {
+  let filePath = getFilePath(req);
+  if (fs.existsSync(filePath)) {
+    let fileContents = fs.readFileSync(filePath)
+    res.setHeader('Content-type', setContentType(filePath));
+    res.write(fileContents);
+    res.end();
+  } else {
+    responseError(res);
+  }
+});
+
+const responseError = function(res) {
+  res.statusCode = 404;
+  res.write('file not found');
+  res.end();
+};
+
+const PORT = 5000;
+let server = http.createServer(app);
+server.on('error', e => console.error('**error**', e.message));
+server.listen(PORT, (e) => console.log(`server listening at ${PORT}`));
---
+++ webapp.js
@@ -0,0 +1,120 @@
+const toKeyValue = kv => {
+  let parts = kv.split('=');
+  return {
+    key: parts[0].trim(),
+    value: parts[1].trim()
+  };
+};
+
+const accumulate = (o, kv) => {
+  o[kv.key] = kv.value;
+  return o;
+};
+
+const parseBody = function (text){
+  return text && text.split('&').map(toKeyValue).reduce(accumulate, {}) || {};
+};
+
+let redirect = function(path) {
+  console.log(`redirecting to ${path}`);
+  this.statusCode = 302;
+  this.setHeader('location', path);
+  this.end();
+};
+
+const parseCookies = text => {
+  try {
+    return text && text.split(';').map(toKeyValue).reduce(accumulate, {}) || {};
+  } catch (e) {
+    return {};
+  };
+};
+
+let invoke = function(req, res) {
+  let handler = this._handlers[req.method][req.url];
+  if (!handler) {
+    res.end();
+    return;
+  };
+  handler(req, res);
+};
+
+const initialize = function() {
+  this._handlers = {
+    GET: {},
+    POST: {}
+  };
+  this._preprocess = [];
+  this._postprocess = [];
+};
+
+const get = function(url, handler) {
+  this._handlers.GET[url] = handler;
+}
+
+const post = function(url, handler) {
+  this._handlers.POST[url] = handler;
+};
+
+const use = function(handler) {
+  this._preprocess.push(handler);
+};
+
+const postuse = function(handler) {
+  this._postprocess.push(handler);
+}
+
+const urlIsOneOf = function(urls) {
+  return urls.includes(this.url);
+};
+
+const parseCookiesAndBindRes = function (req,res) {
+  res.redirect = redirect.bind(res);
+  req.urlIsOneOf = urlIsOneOf.bind(req);
+  req.cookies = parseCookies(req.headers.cookie || '');
+};
+
+const postprocess = function (req,res) {
+  this._postprocess.forEach(middleware => {
+    middleware(req, res);
+    return;
+  });
+};
+
+const preprocess = function (req,res,content) {
+  req.body = parseBody(content);
+  content = "";
+  this._preprocess.forEach(middleware => {
+    if (res.finished) return;
+    middleware(req, res);
+  });
+};
+
+const main = function(req, res) {
+  parseCookiesAndBindRes(req,res);
+  let content = "";
+  req.on('data', data => content += data.toString());
+  req.on('end', () => {
+    preprocess.call(this,req,res,content);
+    let urlList = ["/guestBook.html","/login","/logout"];
+    if (urlList.includes(req.url)) {
+      invoke.call(this, req, res);
+      return;
+    } else {
+      postprocess.call(this,req,res);
+    }
+  });
+};
+
+let create = () => {
+  let rh = (req, res) => {
+    main.call(rh, req, res);
+  };
+  initialize.call(rh);
+  rh.get = get;
+  rh.post = post;
+  rh.use = use;
+  rh.postuse = postuse;
+  return rh;
+};
+exports.create = create;
